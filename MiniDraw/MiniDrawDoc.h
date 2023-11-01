3950acf086ce56407081c8383513fd00f7828226
MaksimYurau <2004softwaredeveloper@gmail.com>
2023-11-01 22:20:03 +0300

    Добавьте файлы проекта.

---
+++ MiniDraw/MiniDrawDoc.h
@@ -0,0 +1,69 @@
+﻿
+// MiniDrawDoc.h: интерфейс класса CMiniDrawDoc 
+//
+
+#include "Afxtempl.h" // шаблоны библиотеки MFC (например, для использования шаблона класса CTypedPtrArray)
+
+#pragma once
+
+class CLine : public CObject
+{
+protected:
+	int m_X1, m_Y1, m_X2, m_Y2;
+public:
+	CLine(int X1, int Y1, int X2, int Y2) 
+	{
+		m_X1 = X1; 
+		m_Y1 = Y1;
+		m_X2 = X2;
+		m_Y2 = Y2;
+	}
+	void Draw(CDC* PDC);
+};
+
+class CMiniDrawDoc : public CDocument
+{
+protected: // создать только из сериализации
+	CTypedPtrArray<CObArray, CLine*> m_LineArray;
+
+	CMiniDrawDoc() noexcept;
+	DECLARE_DYNCREATE(CMiniDrawDoc)
+
+// Атрибуты
+public:
+	
+// Операции
+public:
+	void AddLine(int X1, int Y1, int X2, int Y2);
+	CLine* GetLine(int Index);
+	int GetNumLines();
+	// остальные определения класса CMiniDrawDoc.h
+	
+// Переопределение
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// Реализация
+public:
+	virtual ~CMiniDrawDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Созданные функции схемы сообщений
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// Вспомогательная функция, задающая содержимое поиска для обработчика поиска
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
---
+++ MiniDraw/MiniDrawView.cpp
@@ -0,0 +1,175 @@
+﻿
+// MiniDrawView.cpp: реализация класса CMiniDrawView
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS можно определить в обработчиках фильтров просмотра реализации проекта ATL, эскизов
+// и поиска; позволяет совместно использовать код документа в данным проекте.
+#ifndef SHARED_HANDLERS
+#include "MiniDraw.h"
+#endif
+
+#include "MiniDrawDoc.h"
+#include "MiniDrawView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMiniDrawView
+
+IMPLEMENT_DYNCREATE(CMiniDrawView, CView)
+
+BEGIN_MESSAGE_MAP(CMiniDrawView, CView)
+	// Стандартные команды печати
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMiniDrawView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+
+    ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONUP()
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// Создание или уничтожение CMiniDrawView
+
+CMiniDrawView::CMiniDrawView() noexcept
+{
+	// TODO: добавьте код создания
+	m_Dragging = 0;
+	m_HCross = AfxGetApp()->LoadStandardCursor(IDC_CROSS);
+}
+
+CMiniDrawView::~CMiniDrawView()
+{
+}
+
+BOOL CMiniDrawView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: изменить класс Window или стили посредством изменения
+	//  CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// Рисование CMiniDrawView
+
+void CMiniDrawView::OnDraw(CDC* /*pDC*/)
+{
+	CMiniDrawDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: добавьте здесь код отрисовки для собственных данных
+}
+
+
+// Печать CMiniDrawView
+
+
+void CMiniDrawView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMiniDrawView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// подготовка по умолчанию
+	return DoPreparePrinting(pInfo);
+}
+
+void CMiniDrawView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: добавьте дополнительную инициализацию перед печатью
+}
+
+void CMiniDrawView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: добавьте очистку после печати
+}
+
+void CMiniDrawView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMiniDrawView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// Диагностика CMiniDrawView
+
+#ifdef _DEBUG
+void CMiniDrawView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMiniDrawView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMiniDrawDoc* CMiniDrawView::GetDocument() const // встроена неотлаженная версия
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMiniDrawDoc)));
+	return (CMiniDrawDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Обработчики сообщений CMiniDrawView
+
+
+void CMiniDrawView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: добавьте свой код обработчика сообщений или вызов стандартного
+
+	m_PointOld = point;
+	m_PointOrigin = point;
+
+	SetCapture();
+	m_Dragging = 1;
+
+	RECT Rect;
+	GetClientRect(&Rect);
+	ClientToScreen(&Rect);
+	::ClipCursor(&Rect);
+
+	CView::OnLButtonDown(nFlags, point);
+}
+
+void CMiniDrawView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Здесь добавьте собственный код обработчика
+	// и / или вызов стандартного обработчика
+
+	if (nFlags &MK_SHIFT)
+	// клавиша Shift была нажата при перемещении мыши
+
+	::SetCursor(m_HCross);
+	if (m_Dragging)
+	{
+		CClientDC ClientDC(this);
+		ClientDC.SetROP2(R2_NOT);
+		ClientDC.MoveTo(m_PointOrigin);
+		ClientDC.LineTo(m_PointOld);
+		ClientDC.MoveTo(m_PointOrigin);
+		ClientDC.LineTo(point);
+		m_PointOld = point;
+	}
+
+	CView::OnMouseMove(nFlags, point);
+}
